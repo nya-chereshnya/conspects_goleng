e858404d89d356e3880574a4861edd82d488d152
Pavel Kuldyshev <pk@fabuza.ru>
2023-11-13 20:14:48 +0300

    Add channels; Add 201 - Alan A.A. Donovan, Brian W. Kernighan, The GO Programming Language

---
+++ 201-command-line/echo/echo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+  start := time.Now()
+  // ECHO 1
+  var s, sep string
+  for i:= 1; i < len(os.Args); i++ {
+    s64 := strconv.FormatFloat(time.Since(start).Seconds(), 'f', -1, 64)
+    s += sep + s64 + " " + os.Args[i]
+    sep = "\n"
+  }
+  fmt.Println(s)
+
+  //ECHO 2
+  // var s, sep string
+  // for _, arg := range os.Args[1:] {
+  //   s64 := strconv.FormatFloat(time.Since(start).Seconds(), 'f', -1, 64)
+  //   s += sep + s64 + " " + arg
+  //   sep = "\n"
+  // }
+  // fmt.Println(s)
+
+  // ECHO 3
+  // fmt.Println(strings.Join(os.Args[1:], " "))
+  // fmt.Println(os.Args[0:])
+}
